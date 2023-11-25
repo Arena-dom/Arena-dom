c7115456f9c0ab745f590d1ec84c24a0a142416e
Zuzanna Konieczna <zuzannakonieczna29@gmail.com>
2023-11-25 02:49:25 +0100

    changes

---
+++ constants/animationDuration.tsx
@@ -0,0 +1,26 @@
+interface AnimationDurations {
+    fast: number;
+    normal: number;
+    slow: number;
+    fadeIn: number;
+    fadeOut: number;
+    slideIn: number;
+    slideOut: number;
+    buttonPress: number;
+    modalEnter: number;
+    modalExit: number;
+  }
+  
+  export const animationDurations: AnimationDurations = {
+    fast: 150, 
+    normal: 300, 
+    slow: 500, 
+    fadeIn: 200, 
+    fadeOut: 200, 
+    slideIn: 350, 
+    slideOut: 350,
+    buttonPress: 100, 
+    modalEnter: 400, 
+    modalExit: 400, 
+  };
+  
--- themes/theme.js
+++ themes/theme.js
@@ -1,9 +1,40 @@
 export const theme = {
     fonts: {
       HeaderRegular: 'Roboto-Regular', //heading
-      TextMedium: 'Roboto-Medium',
-      bodyBold: 'Roboto-Bold',
-      accentSemiBold: 'Montserrat-SemiBold',  //text
+      HeaderBlack: 'Roboto-Black',
+      HeaderBlackItalic: 'Roboto-BlackItalic',
+      HeaderBold: 'Roboto-Bold',
+      HeaderItalic: 'Roboto-Italic',
+      HeadeLight: 'Roboto-Light',
+      HeaderLightItalic: 'Roboto-LightItalic',
+      HeaderMedium: 'Roboto-Medium',
+      HeaderMediumItalic: 'Roboto-MediumItalic',
+      HeaderThin: 'Roboto-Thin',
+      HeaderThinItallic: 'Roboto-ThinItalic',
+      TextBlack: 'Montserrat-Black',
+      TextBlackItalic: 'Montserrat-BlackItalic',
+      TextBold: 'Montserrat-Bold',
+      TextBoldItalic: 'Montserrat-BoldItalic',
+      TextExtraBold: 'Monstserrat-ExtraBold',
+      TextExtraBoldItalic: 'Monstserrat-ExtraBoldItalic',
+      TextExtraLight: 'Monstserrat-ExtraLight',
+      TextExtraLightItalic: 'Monstserrat-ExtraLightItalic',
+      TextMedium: 'Monstserrat-Medium',
+      TextMediumItalic: 'Monstserrat-MediumItalic',
+      TextRegular:  'Monstserrat-Regular',
+      TextSemiBold: 'Monstserrat-SemiBold',
+      TextSemiBoldItalic: 'Monstserrat-SemiBoldItalic',
+      TextThin: 'Monsteserrat-Thin',
+      TextThinItalic: 'Monstserrat-ThinItalic',
+      BodyBlack: 'Lato-Black',
+      BodyBlackItalic: 'Lato-BlackItalic',
+      BodyBold: 'Lato-Bold',
+      BodyBoldItalic: 'Lato-BoldItalic',
+      BodyLight: 'Lato-Light',
+      BodyLightItalic: 'Lato-LightItalic',
+      BodyRegular: 'Lato-Regular',
+      BodyThin: 'Lato-Thin',
+      BodyThinItalic: 'Lato-ThinItalic'
     },
     // ... Add other theme-related styling here
   };
